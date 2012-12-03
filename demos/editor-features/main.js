28f7b16606d2a07a5f26338bdb29754174607710
Dimitar Bonev <dsbonev@gmail.com>
2012-12-03 22:56:21 +0200

    add Saving files slide

---
+++ demos/editor-features/main.js
@@ -0,0 +1,6 @@
+window.addEventListener('input', function (event) {
+  'use strict';
+  console.log('input listener executed', event.target.valueAsNumber); //test event propagation
+  //var $ = document.querySelector.bind(document);
+  //$('#result').value = $('#a').valueAsNumber + $('#b').valueAsNumber;
+}, false);​
